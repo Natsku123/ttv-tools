b97528f1c0d9b424d2fe0c77ebe36c9df1b4de4b
Max Mecklin <max@meckl.in>
2023-06-11 23:11:28 +0300

    Change to SessionLocal

--- backend/core/database/__init__.py
+++ backend/core/database/__init__.py
@@ -21,4 +21,4 @@ SQLALCHEMY_DATABASE_URL = "postgresql://{username}:{password}@{server}/{db}" \
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
