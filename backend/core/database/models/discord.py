e6727ae048a3c9bd96e2c7cadefbd9e4aace5676
Max Mecklin <max@meckl.in>
2024-12-21 21:57:47 +0200

    Fix discord ids from api

--- backend/core/database/models/discord.py
+++ backend/core/database/models/discord.py
@@ -2,20 +2,20 @@ from pydantic import BaseModel
 
 
 class DiscordRole(BaseModel):
-    discord_id: int
+    discord_id: str
     name: str
     color: tuple[int, int, int]
     mention: str
 
 
 class DiscordChannel(BaseModel):
-    discord_id: int
+    discord_id: str
     name: str
     jump_url: str
 
 
 class DiscordUser(BaseModel):
-    discord_id: int
+    discord_id: str
     avatar_url: str | None
     name: str
     mention: str
@@ -23,7 +23,7 @@ class DiscordUser(BaseModel):
 
 
 class DiscordServer(BaseModel):
-    discord_id: int
+    discord_id: str
     name: str
     icon_url: str | None
     description: str | None
--- backend/core/routes/discord.py
+++ backend/core/routes/discord.py
@@ -7,14 +7,13 @@ from core.database.models.users import User
 from core.deps import get_current_user, get_ipc
 from core.routes import not_authorized, not_found, forbidden
 
-from config import settings
-
-
 router = APIRouter()
 
 
 @router.get("/servers", response_model=list[DiscordServer], tags=["discord"])
-async def get_servers(*, current_user: User = Depends(get_current_user), ipc: Client = Depends(get_ipc)) -> list[DiscordServer]:
+async def get_servers(
+    *, current_user: User = Depends(get_current_user), ipc: Client = Depends(get_ipc)
+) -> list[DiscordServer]:
     if not current_user:
         raise not_authorized()
 
@@ -22,30 +21,27 @@ async def get_servers(*, current_user: User = Depends(get_current_user), ipc: Cl
         raise HTTPException(status_code=400, detail="Discord not linked!")
 
     if current_user.is_superadmin:
-        servers = await ipc.request(
-            "get_all_servers"
-        )
+        servers = await ipc.request("get_all_servers")
     else:
-        servers = await ipc.request(
-            "get_user_servers",
-            user_id=current_user.discord_id
-        )
+        servers = await ipc.request("get_user_servers", user_id=current_user.discord_id)
 
     return [DiscordServer.parse_obj(s) for s in servers]
 
 
 @router.get("/servers/{server_id}", response_model=DiscordServer, tags=["discord"])
-async def get_server(*, current_user: User = Depends(get_current_user), ipc: Client = Depends(get_ipc), server_id: str = Path(..., description="ID of server")) -> DiscordServer:
+async def get_server(
+    *,
+    current_user: User = Depends(get_current_user),
+    ipc: Client = Depends(get_ipc),
+    server_id: str = Path(..., description="ID of server")
+) -> DiscordServer:
     if not current_user:
         raise not_authorized()
 
     if not current_user.discord_id:
         raise HTTPException(status_code=400, detail="Discord not linked!")
 
-    server = await ipc.request(
-        "get_server",
-        server_id=server_id
-    )
+    server = await ipc.request("get_server", server_id=server_id)
 
     if server == {}:
         raise not_found("Discord Server")
